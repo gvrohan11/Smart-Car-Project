b11ba5adb54340d49471ad8fd3ec45596169ef33
Rohan G <gvrohan11@gmail.com>
2024-01-01 17:54:21 +0530

    changes

--- Cloud/app.py
+++ Cloud/app.py
@@ -3,18 +3,35 @@ from flask import Flask, jsonify, render_template, request, send_file, url_for, 
 
 app = Flask(__name__)
 
+cached_direction = ""
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/control/<direction>', methods=['POST'])
-def control_car(direction):
+def controlCar(direction):
+
+    global cached_direction
 
     try:
         print(f"Direction: {direction}")
+        cached_direction = direction
         return jsonify({'direction': direction}), 200
     except:
         return jsonify({'error': 'Something went wrong'}), 400
+
+@app.route('/getDirection', methods=["GET"])
+def getDirection():
+
+    global cached_direction
+
+    try:
+        return jsonify({"Direction" : cached_direction}), 200
+    except:
+        return "error", 400
+    
+
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
--- Raspberrypi/receiver.py
+++ Raspberrypi/receiver.py
@@ -4,27 +4,31 @@ import time
 # Cloud server endpoint
 CLOUD_SERVER_URL = "something"
 
-def move(direction):
-    # include movement logistics here
-    print(direction)
-    pass
-
 response = []
 
-control_directions = {}
+direction = ""
+
+def move(direction):
+    print(direction)
+    # include movement logistics here
+    
 
 def send_request():
+
+    global response, direction
+
     try:
         # Send a GET request to the cloud server
-        response = requests.get(CLOUD_SERVER_URL)
+        response = requests.get(f"{CLOUD_SERVER_URL}/getDirection")
         
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Parse the response to get control directions
-            control_directions = response.json()
+            json = response.json()
+            direction = json["Direction"]
             
             # Process the control directions (this is where you would move the car)
-            move(control_directions)
+            move(direction)
     except:
         print(f"Error getting info {response.status_code}")
 
